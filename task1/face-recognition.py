b539b544dc599ec749db26112570aea123137c08
Gabriel_Freeze <gabriel.hili.20@um.edu.mt>
2021-11-20 13:20:14 +0100

    Minor Changes

--- task1/face-recognition.py
+++ task1/face-recognition.py
@@ -9,9 +9,10 @@ while cap.isOpened():
     if ret:
         frame = cv2.flip(frame, 1)                                                          #Flip the frame horizontally.
         frameSmall = cv2.resize(frame, (0,0), None, 1/scl, 1/scl)                           #Resize the frame by a factor of scl.
+        frameSmall = cv2.cvtColor(frameSmall, cv2.COLOR_BGR2GRAY)                           #Convert to grayscale.
+        frameSmall = cv2.equalizeHist(frameSmall)                                           #Equalize the histogram.
 
         face_cascade = cv2.CascadeClassifier('cascade/cascade.xml')                         #Load the cascade.
-        frameSmall = cv2.cvtColor(frameSmall, cv2.COLOR_BGR2GRAY)                           #Convert to grayscale.
         faces = face_cascade.detectMultiScale(frameSmall, scaleFactor=1.05, minNeighbors=3) #Detect faces.
         
         for (x, y, w, h) in [i*scl for i in faces]:                                         #Draw rectangle around the faces.
