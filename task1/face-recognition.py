cfc5da47f312a4fe80b1ff143467f271ba76a4dd
Gabriel_Freeze <gabriel.hili.20@um.edu.mt>
2021-11-26 18:24:01 +0100

    Finishing Touches

--- task1/face-recognition.py
+++ task1/face-recognition.py
@@ -12,7 +12,8 @@ while cap.isOpened():
         frameSmall = cv2.cvtColor(frameSmall, cv2.COLOR_BGR2GRAY)                           #Convert to grayscale.
         frameSmall = cv2.equalizeHist(frameSmall)                                           #Equalize the histogram.
 
-        face_cascade = cv2.CascadeClassifier('cascade/cascade.xml')                         #Load the cascade.
+        try: face_cascade = cv2.CascadeClassifier('cascade/cascade.xml')                    #Load the cascade (Linux).
+        except: face_cascade = cv2.CascadeClassifier('cascade\\cascade.xml')                #Load the cascade (Windows).
         faces = face_cascade.detectMultiScale(frameSmall, scaleFactor=1.05, minNeighbors=3) #Detect faces.
         
         for (x, y, w, h) in [i*scl for i in faces]:                                         #Draw rectangle around the faces.
